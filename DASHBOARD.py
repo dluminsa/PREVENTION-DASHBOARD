9746403f7136422c4a398907131885d272913e29
Desire Luminsa <desireluminsa@gmail.com>
2024-08-21 23:41:02 +0300

    first commit

---
+++ DASHBOARD.py
@@ -0,0 +1,199 @@
+import pandas as pd 
+import streamlit as st 
+import os
+import numpy as np
+import random
+import time
+from pathlib import Path
+from streamlit_gsheets import GSheetsConnection
+import plotly.express as px
+import plotly.graph_objects as go
+
+st.set_page_config(
+     page_title= 'PREVENTION ACTIVITIES DASHBOARD'
+)
+cola,colb,colc = st.columns([1,2,1])
+cola.write('')
+colb.markdown("<h4><b>PREVENTION ACTIVITIES DASHBOARD</b></h4>", unsafe_allow_html=True)
+colc.write('')
+current_time = time.localtime()
+k = time.strftime("%V", current_time)
+t = int(k) + 13
+cola,colb,colc = st.columns([1,2,1])
+cola.write(f'**CURRENT WEEK IS: {k}**')
+colc.write(f'**SURGE WEEK IS: {t}**')
+try:
+     conn = st.connection('gsheets', type=GSheetsConnection)     
+     dfb = conn.read(worksheet='DONE', usecols=list(range(11)), ttl=5)
+     dfb = dfb.dropna(how='all')
+except:
+     st.write(f"**Your network is poor, couldn't connect to the google sheet**")
+     st.write(f"**TRY AGAIN WITH BETTER INTERNET**")
+     st.stop()
+
+dfb= dfb[['CLUSTER','DISTRICT','ACTIVITY', 'DONE', 'WEEK','FACILITY']]
+file = r'PREVENTION.csv'
+dfa = pd.read_csv(file)
+
+
+dfa= dfa[['DISTRICT','ACTIVITY', 'PLANNED']]
+
+dfb['WEEK'] = dfb['WEEK'].astype(int)
+dfb['DISTRICT'] = dfb['DISTRICT'].astype(str)
+#dfb['AREA'] = dfb['AREA'].astype(str)
+dfb['ACTIVITY'] = dfb['ACTIVITY'].astype(str)
+dfb['DONE'] = dfb['DONE'].astype(int)
+
+dfa['DISTRICT'] = dfa['DISTRICT'].astype(str)
+#dfa['AREA'] = dfa['AREA'].astype(str)
+dfa['ACTIVITY'] = dfa['ACTIVITY'].astype(str)
+dfa['PLANNED'] = dfa['PLANNED'].astype(int)
+
+
+st.sidebar.subheader('Filter from here ')
+district = st.sidebar.multiselect('Pick a district', dfa['DISTRICT'].unique())
+
+if not district:
+    dfa2 = dfa.copy()
+    dfb2 = dfb.copy()
+else:
+    dfa2 = dfa[dfa['DISTRICT'].isin(district)]
+    dfb2 = dfb[dfb['DISTRICT'].isin(district)]
+
+# #create for district
+# area = st.sidebar.multiselect('Choose a thematic area', dfa2['AREA'].unique())
+# if not area:
+#     dfa3 = dfa2.copy()
+#     dfb3 = dfb2.copy()
+# else:
+#     dfa3 = dfa2[dfa2['AREA'].isin(area)]
+#     dfb3 = dfb2[dfb2['AREA'].isin(area)]
+ 
+#for facility
+activity = st.sidebar.multiselect('Choose an activity', dfa2['ACTIVITY'].unique())
+
+#Filter Week, District, Facility
+if not district  and not activity:
+    filtered_dfa = dfa
+    filtered_dfb = dfb
+elif not activity:
+    filtered_dfa = dfa[dfa['DISTRICT'].isin(district)].copy()
+    filtered_dfb = dfb[dfb['DISTRICT'].isin(district)].copy()
+# elif not district and not activity:
+#     filtered_dfa = dfa[dfa['AREA'].isin(area)].copy()
+#     filtered_dfb = dfb[dfb['AREA'].isin(area)].copy()
+# elif area and activity:
+#      #
+#     filtered_dfa = dfa3[dfa3['AREA'].isin(area)& dfa3['ACTIVITY'].isin(activity)].copy()
+#      #
+#     filtered_dfb = dfb3[dfb3['AREA'].isin(area)& dfb3['ACTIVITY'].isin(activity)].copy()
+elif district and activity:
+     #
+    filtered_dfa = dfa2[dfa2['DISTRICT'].isin(district)& dfa2['ACTIVITY'].isin(activity)].copy()
+     #
+    filtered_dfb = dfb2[dfb2['DISTRICT'].isin(district)& dfb2['ACTIVITY'].isin(activity)].copy()
+# elif district and area:
+#      #
+#     filtered_dfa = dfa3[dfa3['DISTRICT'].isin(district)& dfa3['AREA'].isin(area)].copy()
+#      #
+#     filtered_dfb = dfb3[dfb3['DISTRICT'].isin(district)& dfb3['AREA'].isin(area)].copy()
+elif activity:
+    filtered_dfa = dfa2[dfa2['ACTIVITY'].isin(activity)].copy()
+    filtered_dfb = dfb2[dfb2['ACTIVITY'].isin(activity)].copy()
+# else:
+#     filtered_dfa = dfa2[dfa2['DISTRICT'].isin(district) & dfa2['ACTIVITY'].isin(activity)].copy()
+#     filtered_dfb = dfb2[dfb2['DISTRICT'].isin(district) & dfb2['ACTIVITY'].isin(activity)].copy()
+#################################################################################################
+cols,cold = st.columns(2)
+dist = filtered_dfb['DISTRICT']. unique()
+if not district:
+    pass
+elif len(dist) == 0:
+    cols.write(f'**No data for this district**')
+else:
+    cols.write(f'**You are viewing data for: {dist}**')
+
+ar = filtered_dfb['AREA']. unique()
+if len(ar) == 0:
+    cold.write(f'**No data for the thematic area(s) chosen**')
+elif len(ar)>1:
+     pass
+elif len(ar)==1:
+     cold.write(f'**The data set is filtered by: {ar} thematic area(s)**')
+
+act = filtered_dfb['ACTIVITY']. unique()
+
+if not activity:
+    st.write(f'**No specific activity has been chosen**')
+elif len(act) == 0:
+    st.write(f'**No data for the the activity chosen**')
+else:
+    st.write(f'**ACTIVITIES INCLUDED ARE: {act}**')
+
+plan = filtered_dfa['PLANNED'].sum()
+conducted = filtered_dfb['DONE'].sum()
+notdone = plan - conducted
+if plan ==0:
+     perc =0
+else:
+    perc = round((conducted/plan)*100)
+
+if conducted>plan:
+    st.warning(f"SOMETHING IS WRONG, IT SEEMS ACTIVITIES DONE ARE MORE THAN THOSE THAT WERE PLANNED FOR!!")
+
+col1,col2,col3,col4 = st.columns(4, gap='large')
+
+with col1:
+    st.metric(label='**PLANNED**', value=f'{plan:,.0f}')
+with col2:
+    st.metric(label='**CONDUCTED**', value=f'{conducted:,.0f}')
+with col3:
+    st.metric(label='**PERCENTAGE**', value=f'{perc:,.0f}')
+with col4:
+    st.metric(label='**NOT DONE**', value=f'{notdone:,.0f}')
+
+#######################################################################################################
+#PIE CHART
+#st.divider()
+col1, col2,col3 = st.columns([1,4,1])
+labels = ['DONE', 'NOT DONE']
+# Values
+values = [conducted, notdone]
+colors = ['blue', 'red']
+# Creating the pie chart with specified colors and hole
+fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+value', 
+                             insidetextorientation='radial', marker=dict(colors=colors), hole=0.4)])
+
+# Updating the layout for better readability
+fig.update_traces(textposition='inside', textfont_size=20)
+fig.update_layout(title_text='DONE vs NOT DONE', title_x=0.3)
+
+col1, col2,col3 = st.columns([1,4,1])
+with col2:
+     st.plotly_chart(fig, use_container_width=True)
+#############################################################################################
+#LINE GRAPH
+st.divider()
+
+grouped = filtered_dfb.groupby('WEEK').sum(numeric_only=True).reset_index()
+fig2 = px.line(grouped, x='WEEK', y='DONE', title='WEEKLY TRENDS',
+               markers=True)
+
+fig2.update_layout(xaxis_title='WEEK', yaxis_title='TOTAL DONE',
+                    width=800,  # Set the width of the plot
+                     height=400,  # Set the height of the plot
+                     xaxis=dict(showline=True, linewidth=1, linecolor='black',tickmode='linear',tick0=25,dtick=1,),  # Show x-axis line
+                     yaxis=dict(showline=True, linewidth=1, linecolor='black'))  # Show y-axis line)
+
+st.plotly_chart(fig2, use_container_width=True)
+
+filtered_dfc= filtered_dfb[['CLUSTER','DISTRICT','FACILITY' ,'AREA','ACTIVITY', 'DONE', 'WEEK']]
+with st.expander(f'**CLICK HERE TO SEE FULL DATA SET**'):
+    st.dataframe(filtered_dfc.reset_index(drop=True))
+    csv_data = filtered_dfc.to_csv(index=False)
+    st.download_button(
+                        label=" DOWNLOAD THIS DATA SET",
+                        data=csv_data,
+                        file_name="ACTIVITIES.csv",
+                        mime="text/csv")
+    
---
+++ pages/STATUS.py
@@ -0,0 +1,116 @@
+import pandas as pd 
+from datetime import datetime
+from streamlit_gsheets import GSheetsConnection
+import streamlit as st
+import time
+import datetime as dt
+from datetime import datetime, date
+
+
+st.set_page_config(
+     page_title= 'ACTIVITIES REVIEW STATUS'
+)
+st.markdown("<h4><b>CHECK REVIEW STATUS OF PAPER WORK</b></h4>", unsafe_allow_html=True)
+
+total= 0
+district = ''
+cluster = ''
+ids = ''
+Intention = st.radio('**HOW MAY I HELP YOU?**', options=['MARK REVIEWED PAPER WORK', 'CHECK REVIEW STATUS'], index=None)
+file = r'PLANNED.csv'
+df = pd.read_csv(file)
+
+if Intention:
+    pass
+else:
+    st.stop()
+
+if Intention == 'MARK REVIEWED PAPER WORK':
+    coln, colm = st.columns([1,2])
+    c = 'HOW MANY DO YOU HAVE?'
+    total = coln.number_input(label=f'**{c}**', value=None, max_value=None, min_value=None,step=1, format="%d")
+    if total:
+         pass
+    else:
+         st.stop()
+    id = []
+    if total:
+        if int(total)==1:     
+            col1, col2,col3 = st.columns(3)
+            m = 'IN PUT PAPER WORK ID'
+            idea = col1.number_input(label=f'**{m}**', value=None, max_value=None, min_value=None,step=1, format="%d")
+            #id.append(ids)
+            col2.write('')
+            col2.write('')
+            submit = col2.button('SUBMIT')
+        else:
+            col1, col2,col3 = st.columns(3)
+            m = 'IN PUT PAPER WORK ID'      
+            for each in range(int(total)):
+                col1, col2,col3 = st.columns(3)
+                every = each+1
+                ids = col1.number_input(label=f'**{m} for paper work {every}**', value=None,key=each, max_value=None, min_value=None,step=1, format="%d")
+                id.append(ids)
+            col2.write('')
+            col2.write('')
+            submit = col2.button('SUBMIT')
+   
+    if submit:
+            if int(total)==1:
+                data1 = pd.DataFrame([{'PAID': idea}])
+            else:                  
+                data1 = pd.DataFrame(id, columns=['PAID'])
+            data2 = data1.dropna(how='all')
+            try:
+                st. write('SUBMITING ID')
+                conn = st.connection('gsheets', type=GSheetsConnection)
+                exist2 = conn.read(worksheet= 'PAID', usecols=list(range(1)),ttl=5)
+                existing2= exist2.dropna(how='all')
+                updated = pd.concat([existing2, data2], ignore_index =True)
+                existing2= exist2.dropna(how='all')
+                conn.update(worksheet = 'PAID', data = updated)         
+                st.success('ID above has been submitted')
+                st.write('RELOADING PAGE')
+                time.sleep(2)
+                st.markdown("""
+                <meta http-equiv="refresh" content="0">
+                    """, unsafe_allow_html=True)
+
+            except:
+                    st.write("Couldn't submit, poor network") 
+                    st.write("Submit again")
+else:
+
+    clusters  = df['CLUSTER'].unique()
+    cluster = st.radio("**Choose a cluster:**", clusters,horizontal=True, index=None)
+    if cluster:
+        try:
+             conn = st.connection('gsheets', type=GSheetsConnection)
+             exist1 = conn.read(worksheet= 'DONE', usecols=list(range(10)),ttl=5)
+             exist2 = conn.read(worksheet= 'PAID', usecols=list(range(2)),ttl=5)
+             existing1= exist1.dropna(how='all')
+             existing2= exist2.dropna(how='all')
+        except:
+             st.write("POOR INTERNET, COULDN'T CONNECT TO THE GOOGLE SHEETS")
+             st.write('Get better internet and try again')
+             st.stop()
+        sent = existing1[existing1['CLUSTER'] == cluster].copy()
+        sent['ID'] =sent['ID'].astype(int)
+        existing2['PAID'] =existing2['PAID'].astype(int)
+        unpaid = sent[~sent['ID'].isin(existing2['PAID'])].copy()
+        a = unpaid.shape[0]
+        if int(a) == 0:
+            st.write('**NO PAPER WORK PENDING**')
+            st.stop()
+        else:
+            unpaid = unpaid[['DISTRICT', 'FACILITY', 'ACTIVITY', 'ID','START DATE', 'END DATE', 'DATE OF SUBMISSION']]
+            st. markdown(f'YOU HAVE NOT REVIEWED {a} PAPER WORKS')
+            with st.expander('CLICK HERE TO VIEW THEM'):
+                st.write(unpaid)
+    else:
+        st.stop()
+
+
+
+
+
