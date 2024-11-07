e2267317c2b173b9a7a6599dac684a3ef7dd789a
dluminsa <139705111+dluminsa@users.noreply.github.com>
2024-11-07 22:36:42 +0300

    Update DASHBOARD.py

--- DASHBOARD.py
+++ DASHBOARD.py
@@ -88,112 +88,123 @@ elif activity:
     filtered_dfa = dfa2[dfa2['ACTIVITY'].isin(activity)].copy()
     filtered_dfb = dfb2[dfb2['ACTIVITY'].isin(activity)].copy()
     filtered_dfm = dfm2[dfm2['ACTIVITY'].isin(activity)].copy()
+################################################################################################
+#FACILITY
+facilities = filtered_dfb['FACILITY'].unique()
+st.sidebar.subheader('CHECK FACILITY PERFORMANCE ')
+fac = st.sidebar.multiselect('Pick a cluster', facilities)
+if fac:
+     facd = filtered_dfb[filtered_dfb['FACILITY']== fac].copy()
+     conducted = facd['DONE'].sum()
+     st.write(f'FOR THE SELECTED ACTIVITY, {fac} HAS CONDUCTED {conducted}')
+else pass:
 #################################################################################################
-cols,cold = st.columns(2)
-clus = filtered_dfb['CLUSTER']. unique()
-if not cluster:
-    pass
-elif len(clus) == 0:
-    cols.write(f'**No data for this cluster**')
-else:
-    cols.write(f'**You are viewing data for: {clus}**')
-
-act = filtered_dfb['ACTIVITY']. unique()
-
-if not activity:
-    st.write(f'**No specific activity has been chosen**')
-elif len(act) == 0:
-    st.write(f'**No data for the the activity chosen**')
-else:
-    st.write(f'**ACTIVITIES INCLUDED ARE: {act}**')
-
-plan = filtered_dfa['PLANNED'].sum()
-conducted = filtered_dfb['DONE'].sum()
-notdone = plan - conducted
-if plan ==0:
-     perc =0
-else:
-    perc = round((conducted/plan)*100)
-
-exp = round((int(t)/12)*100)
-if conducted>plan:
-    st.warning(f"SOMETHING IS WRONG, IT SEEMS ACTIVITIES DONE ARE MORE THAN THOSE THAT WERE PLANNED FOR!!")
-
-col1,col2,col3,col4, col5 = st.columns(5)#, gap='large')
-
-with col1:
-    st.metric(label='**PLANNED**', value=f'{plan:,.0f}')
-with col2:
-    st.metric(label='**DONE**', value=f'{conducted:,.0f}')
-with col3:
-    st.metric(label='**%AGE**', value=f'{perc:,.0f}')
-with col4:
-    st.metric(label='**EXPECTED**', value=f'{exp} %')
-with col5:
-    st.metric(label='**BALANCE**', value=f'{notdone:,.0f}')
-#####################################################################################################f
-
-plan = filtered_dfa['AMOUNT'].sum()
-conducted = filtered_dfm['AMOUNT'].sum()
-notdone = plan - conducted
-
-if conducted>plan:
-    st.warning(f"SOMETHING IS WRONG, IT SEEMS ACTIVITIES DONE ARE MORE THAN THOSE THAT WERE PLANNED FOR!!")
-
-col1,col2,col3 = st.columns(3)#, gap='large')
-with st.expander('CLICK HERE TO SEE EXPENDITURE'):
-     col1,col2,col3 = st.columns(3)#,
+if not fac:
+     cols,cold = st.columns(2)
+     clus = filtered_dfb['CLUSTER']. unique()
+     if not cluster:
+         pass
+     elif len(clus) == 0:
+         cols.write(f'**No data for this cluster**')
+     else:
+         cols.write(f'**You are viewing data for: {clus}**')
+     
+     act = filtered_dfb['ACTIVITY']. unique()
+     
+     if not activity:
+         st.write(f'**No specific activity has been chosen**')
+     elif len(act) == 0:
+         st.write(f'**No data for the the activity chosen**')
+     else:
+         st.write(f'**ACTIVITIES INCLUDED ARE: {act}**')
+     
+     plan = filtered_dfa['PLANNED'].sum()
+     conducted = filtered_dfb['DONE'].sum()
+     notdone = plan - conducted
+     if plan ==0:
+          perc =0
+     else:
+         perc = round((conducted/plan)*100)
+     
+     exp = round((int(t)/12)*100)
+     if conducted>plan:
+         st.warning(f"SOMETHING IS WRONG, IT SEEMS ACTIVITIES DONE ARE MORE THAN THOSE THAT WERE PLANNED FOR!!")
+     
+     col1,col2,col3,col4, col5 = st.columns(5)#, gap='large')
+     
      with col1:
-         st.metric(label='**BUDGETED**', value=f'{plan:,.0f}')
+         st.metric(label='**PLANNED**', value=f'{plan:,.0f}')
      with col2:
-         st.metric(label='**SPENT**', value=f'{conducted:,.0f}')
+         st.metric(label='**DONE**', value=f'{conducted:,.0f}')
      with col3:
+         st.metric(label='**%AGE**', value=f'{perc:,.0f}')
+     with col4:
+         st.metric(label='**EXPECTED**', value=f'{exp} %')
+     with col5:
          st.metric(label='**BALANCE**', value=f'{notdone:,.0f}')
-
-#######################################################################################################
-#PIE CHART
-#st.divider()
-col1, col2,col3 = st.columns([1,4,1])
-labels = ['DONE', 'NOT DONE']
-# Values
-values = [conducted, notdone]
-colors = ['blue', 'red']
-# Creating the pie chart with specified colors and hole
-fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+value', 
-                             insidetextorientation='radial', marker=dict(colors=colors), hole=0.4)])
-
-# Updating the layout for better readability
-fig.update_traces(textposition='inside', textfont_size=20)
-fig.update_layout(title_text='DONE vs NOT DONE', title_x=0.3)
-
-col1, col2,col3 = st.columns([1,4,1])
-with col2:
-     st.plotly_chart(fig, use_container_width=True)
-#############################################################################################
-#LINE GRAPH
-st.divider()
-
-grouped = filtered_dfb.groupby('WEEK').sum(numeric_only=True).reset_index()
-fig2 = px.line(grouped, x='WEEK', y='DONE', title='WEEKLY TRENDS',
-               markers=True)
-
-fig2.update_layout(xaxis_title='WEEK', yaxis_title='TOTAL DONE',
-                    width=800,  # Set the width of the plot
-                     height=400,  # Set the height of the plot
-                     xaxis=dict(showline=True, linewidth=1, linecolor='black',tickmode='linear',tick0=25,dtick=1,),  # Show x-axis line
-                     yaxis=dict(showline=True, linewidth=1, linecolor='black'))  # Show y-axis line)
-
-st.plotly_chart(fig2, use_container_width=True)
-
-#st.write(filtered_dfb.columns)
-filtered_dfc= filtered_dfb[['CLUSTER','FACILITY','ACTIVITY', 'DONE', 'WEEK','ID']]
-#filtered_dfc = filtered_dfc.set_index('CLUSTER')
-with st.expander(f'**CLICK HERE TO SEE FULL DATA SET**'):
-    st.dataframe(filtered_dfc.reset_index(drop=True))
-    csv_data = filtered_dfc.to_csv(index=False)
-    st.download_button(
-                        label=" DOWNLOAD THIS DATA SET",
-                        data=csv_data,
-                        file_name="ACTIVITIES.csv",
-                        mime="text/csv")
-    
+     #####################################################################################################f
+     
+     plan = filtered_dfa['AMOUNT'].sum()
+     conducted = filtered_dfm['AMOUNT'].sum()
+     notdone = plan - conducted
+     
+     if conducted>plan:
+         st.warning(f"SOMETHING IS WRONG, IT SEEMS ACTIVITIES DONE ARE MORE THAN THOSE THAT WERE PLANNED FOR!!")
+     
+     col1,col2,col3 = st.columns(3)#, gap='large')
+     with st.expander('CLICK HERE TO SEE EXPENDITURE'):
+          col1,col2,col3 = st.columns(3)#,
+          with col1:
+              st.metric(label='**BUDGETED**', value=f'{plan:,.0f}')
+          with col2:
+              st.metric(label='**SPENT**', value=f'{conducted:,.0f}')
+          with col3:
+              st.metric(label='**BALANCE**', value=f'{notdone:,.0f}')
+     
+     #######################################################################################################
+     #PIE CHART
+     #st.divider()
+     col1, col2,col3 = st.columns([1,4,1])
+     labels = ['DONE', 'NOT DONE']
+     # Values
+     values = [conducted, notdone]
+     colors = ['blue', 'red']
+     # Creating the pie chart with specified colors and hole
+     fig = go.Figure(data=[go.Pie(labels=labels, values=values, textinfo='label+value', 
+                                  insidetextorientation='radial', marker=dict(colors=colors), hole=0.4)])
+     
+     # Updating the layout for better readability
+     fig.update_traces(textposition='inside', textfont_size=20)
+     fig.update_layout(title_text='DONE vs NOT DONE', title_x=0.3)
+     
+     col1, col2,col3 = st.columns([1,4,1])
+     with col2:
+          st.plotly_chart(fig, use_container_width=True)
+     #############################################################################################
+     #LINE GRAPH
+     st.divider()
+     
+     grouped = filtered_dfb.groupby('WEEK').sum(numeric_only=True).reset_index()
+     fig2 = px.line(grouped, x='WEEK', y='DONE', title='WEEKLY TRENDS',
+                    markers=True)
+     
+     fig2.update_layout(xaxis_title='WEEK', yaxis_title='TOTAL DONE',
+                         width=800,  # Set the width of the plot
+                          height=400,  # Set the height of the plot
+                          xaxis=dict(showline=True, linewidth=1, linecolor='black',tickmode='linear',tick0=25,dtick=1,),  # Show x-axis line
+                          yaxis=dict(showline=True, linewidth=1, linecolor='black'))  # Show y-axis line)
+     
+     st.plotly_chart(fig2, use_container_width=True)
+     
+     #st.write(filtered_dfb.columns)
+     filtered_dfc= filtered_dfb[['CLUSTER','FACILITY','ACTIVITY', 'DONE', 'WEEK','ID']]
+     #filtered_dfc = filtered_dfc.set_index('CLUSTER')
+     with st.expander(f'**CLICK HERE TO SEE FULL DATA SET**'):
+         st.dataframe(filtered_dfc.reset_index(drop=True))
+         csv_data = filtered_dfc.to_csv(index=False)
+         st.download_button(
+                             label=" DOWNLOAD THIS DATA SET",
+                             data=csv_data,
+                             file_name="ACTIVITIES.csv",
+                             mime="text/csv")
+         
